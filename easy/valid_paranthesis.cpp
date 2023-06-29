2b94955488e1cec99b3d4a85d88e6d3ffe3a1774
Alex Oh <alex@Alexs-Air.hitronhub.home>
2023-06-28 22:29:38 -0700

    commit

---
+++ easy/valid_paranthesis.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        vector<char> stack;
+        for (int i = 0; i < s.length(); ++i) {
+            if (s[i] == '{' || s[i] == '[' || s[i] == '(') {
+                stack.push_back(s[i]);
+            }
+            else {
+                if (stack.size() == 0)
+                {
+                    return false;
+                }
+                if (s[i] - 1 == stack.back() || s[i] - 2 == stack.back()) {
+                    stack.pop_back();
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+        if (stack.size() != 0)
+        {
+            return false;
+        }
+        return true;
+    }
+};
